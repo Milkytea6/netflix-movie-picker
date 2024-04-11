6edb8b0a39ad4d57b3eb7e473f502632bb36fb01
Christopher Giordano <chrisgcpg@gmail.com>
2024-04-10 21:51:54 -0400

    Created a for loop to create and append a div of movie-card to the document.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
-const searchForm =document.getElementById('search-form');
+const searchForm = document.getElementById('search-form');
 const movieButton = document.getElementById('movie-button');
+const movieResults = document.getElementById('movie-results');
 
 const options = {
     method: 'GET',
@@ -8,21 +9,30 @@ const options = {
         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
     }
 };
-async function getStreamingData(url){
+async function getStreamingData(url) {
     try {
         console.log(url);
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
         console.log(result);
+        for (let i = 0; i < result.length; i++) {
+            console.log(result[i]);
+            const movieCard = document.createElement('div').classList.add('movie-card').innerhtml('title');
+            movieResults.append(movieCard);
+        }
     } catch (error) {
         console.error(error);
     }
+
+
+
 }
-searchForm.addEventListener('submit', function(event){
+
+searchForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     const searchInput = document.getElementById('search-bar').value;
-    
+
     const url = `https://streaming-availability.p.rapidapi.com/search/title?title=${searchInput}&country=us&show_type=all&output_language=en`;
     console.log(url);
     getStreamingData(url);
