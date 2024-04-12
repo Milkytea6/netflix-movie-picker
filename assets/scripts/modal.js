76f382fa0d6c819556e6acf767d04eae0633e60f
Daniel Nunez <161102831+danie1nunez@users.noreply.github.com>
2024-04-12 19:58:49 -0400

    added data attribute values to the genre buttons so that they correlate to the movie genre api.

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,6 +1,18 @@
 const modalButton= document.getElementById('movie-button');
 const modal = document.querySelector('.ui.modal');
+const genreButtons= document.querySelectorAll('.genre-container #genres')
 
 modalButton.addEventListener('click', function(){
     modal.classList.add('active');
 })
+
+genreButtons.forEach(function(button) {
+    // Add event listener for each button (e.g., click event)
+    button.addEventListener('click', function() {
+        // Retrieve the API value attached to the button
+        const apiValue = button.getAttribute('data-result-api');
+        console.log(apiValue);
+        // Your code here for using the API value
+        console.log('API value attached to button: ' + apiValue);
+    });
+});
