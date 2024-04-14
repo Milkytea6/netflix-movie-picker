993e806d901084386f99d8fcb5aa042bf0bf7368
Christopher Giordano <chrisgcpg@gmail.com>
2024-04-14 13:10:52 -0400

    Added a function to the genre button so the user can search by genre. Added a type button to the modal to search by movie or series. Added more code to the genre buttons so they can be added the the fetch request.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,5 @@
 const searchForm = document.getElementById('search-form');
 const movieResults = document.getElementById('movie-results');
-
 const options = {
     method: 'GET',
     headers: {
@@ -27,14 +26,18 @@ async function getStreamingData(url) {
             const movieTitle = document.createElement('h2');
             movieTitle.classList.add('movie-title');
             movieTitle.textContent = (`${result.result[i].title}`);
+            // Movie type
+            const movieType = document.createElement('p');
+            movieType.classList.add('movie-type');
+            movieType.textContent = (`Type: ${result.result[i].type}`);
             // Movie Genres
             const movieGenre = document.createElement('p');
             movieGenre.classList.add('movie-Genre');
-            movieGenre.textContent = (`${result.result[i].genres.map((genre) => genre.name).join(',')}`);
+            movieGenre.textContent = (`Genre(s): ${result.result[i].genres.map((genre) => genre.name).join(',')}`);
             // Movie Service
             const movieService = document.createElement('p');
             movieService.classList.add('movie-service');
-            const serviceArray = result.result[i].streamingInfo.us
+            const serviceArray = result.result[i].streamingInfo.us;
             // If there is streaming info, then it will itterate over the array returning each unique service that the movie is on.
             if (serviceArray) {
                 let uniqueServices = [];
@@ -44,7 +47,7 @@ async function getStreamingData(url) {
                 uniqueServices = uniqueServices.filter((item,
                     index) => uniqueServices.indexOf(item) === index);
 
-                movieService.textContent = uniqueServices.join(',');
+                movieService.textContent = `Available on: ${uniqueServices.join(',')}`;
             }
 
             else if (!serviceArray) {
@@ -52,7 +55,7 @@ async function getStreamingData(url) {
             }
 
             // Append movie title and genre to card
-            movieCard.append(movieTitle, movieGenre, movieService);
+            movieCard.append(movieTitle, movieType, movieGenre, movieService);
             // Append card to document
             movieResults.append(movieCard);
         }
@@ -79,17 +82,15 @@ searchForm.addEventListener('submit', function (event) {
 
 const modalEl = document.querySelector('main div');
 
-console.log(modalEl);
-modalEl.addEventListener('submit', function (event){
+modalEl.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    movieResults.innerHTML = "";
     const service = 'netflix';// Service Netflix only
     const country = 'us';//us
     const outputLang = 'en';// english
     const orderBy = 'original_title';// sort by tite
     const genreCodes = '10749,35';// genre codes
-    const genreRel  = 'and';// Includes all selected genres
+    const genreRel = 'and';// Includes all selected genres
     const showType = `movie`;// Results are series or movie
 
     const url = `https://streaming-availability.p.rapidapi.com/search/filters?services=${service}&country=${country}&output_language=${outputLang}&order_by=${orderBy}&genres=${genreCodes}&genres_relation=${genreRel}&show_type=${showType}`;
